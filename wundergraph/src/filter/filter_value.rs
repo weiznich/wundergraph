0a50f54a3f38a9552afa41ef3d2ebff230ed7a64
Georg Semmler <georg_semmler_05@web.de>
2018-06-19 14:56:21 +0200

    Add support for some more types
    Now we also support `i16` and `Vec<T>`

--- wundergraph/src/filter/filter_value.rs
+++ wundergraph/src/filter/filter_value.rs
@@ -8,6 +8,11 @@ pub trait FilterValue<C> {
     type AdditionalFilter;
 }
 
+impl<C> FilterValue<C> for i16 {
+    type RawValue = i16;
+    type AdditionalFilter = ();
+}
+
 impl<C> FilterValue<C> for i32 {
     type RawValue = i32;
     type AdditionalFilter = ();
@@ -25,6 +30,14 @@ impl<C> FilterValue<C> for bool {
 
 impl<C> FilterValue<C> for f64 {
     type RawValue = f64;
+    type AdditionalFilter = ();
+}
+
+impl<C, V> FilterValue<C> for Vec<V>
+where
+    V: FromLookAheadValue + FromInputValue + ToInputValue + FilterValue<C> + Clone,
+{
+    type RawValue = Vec<V>;
     type AdditionalFilter = ();
 }
 
