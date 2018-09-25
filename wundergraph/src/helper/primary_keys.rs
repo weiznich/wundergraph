90bdd947797e29033f863c227765d4594b1c83ef
Georg Semmler <georg_semmler_05@web.de>
2018-09-25 11:02:15 +0200

    Use `__diesel_for_each_tuple` to implement tuple related stuff
    Use a internal diesel macro to provide some impl for primary keys in the same way
    diesel provides them for tuples. This also bumps the number of columns
    in primary keys to whatever diesel supports maximally. (It now depents
    on the "table size" feature flags on diesel. Maximally now composite
    primary keys with 128 columns are supported if the right feature flag
    is set)

--- wundergraph/src/helper/primary_keys.rs
+++ wundergraph/src/helper/primary_keys.rs
@@ -23,45 +23,25 @@ impl<'a, A> UnRef<'a> for &'a A {
     }
 }
 
-impl<'a, A> UnRef<'a> for (&'a A,) {
-    type UnRefed = (A,);
+macro_rules! unref_impl {
+    ($(
+        $Tuple:tt {
+            $(($idx: tt) -> $T:ident, $ST: ident, $TT: ident,)+
+        }
+    )+) => {
+        $(
+            impl<'a, $($T,)+> UnRef<'a> for ($(&'a $T,)+) {
+                type UnRefed = ($($T, )+);
 
-    fn as_ref(v: &'a Self::UnRefed) -> Self {
-        (&v.0,)
+                fn as_ref(v: &'a Self::UnRefed) -> Self {
+                    ($(&v.$idx,)+)
+                }
+            }
+        )+
     }
 }
 
-impl<'a, A, B> UnRef<'a> for (&'a A, &'a B) {
-    type UnRefed = (A, B);
-
-    fn as_ref(v: &'a Self::UnRefed) -> Self {
-        (&v.0, &v.1)
-    }
-}
-
-impl<'a, A, B, C> UnRef<'a> for (&'a A, &'a B, &'a C) {
-    type UnRefed = (A, B, C);
-
-    fn as_ref(v: &'a Self::UnRefed) -> Self {
-        (&v.0, &v.1, &v.2)
-    }
-}
-
-impl<'a, A, B, C, D> UnRef<'a> for (&'a A, &'a B, &'a C, &'a D) {
-    type UnRefed = (A, B, C, D);
-
-    fn as_ref(v: &'a Self::UnRefed) -> Self {
-        (&v.0, &v.1, &v.2, &v.3)
-    }
-}
-
-impl<'a, A, B, C, D, E> UnRef<'a> for (&'a A, &'a B, &'a C, &'a D, &'a E) {
-    type UnRefed = (A, B, C, D, E);
-
-    fn as_ref(v: &'a Self::UnRefed) -> Self {
-        (&v.0, &v.1, &v.2, &v.3, &v.4)
-    }
-}
+__diesel_for_each_tuple!(unref_impl);
 
 pub trait PrimaryKeyInputObject<V, I> {
     fn register<'r>(
@@ -110,322 +90,66 @@ where
     }
 }
 
-impl<A, V1, I> PrimaryKeyInputObject<(V1,), I> for (A,)
-where
-    A: Column,
-    V1: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-{
-    fn register<'r>(
-        registry: &mut Registry<'r, WundergraphScalarValue>,
-        info: &I,
-    ) -> Vec<Argument<'r, WundergraphScalarValue>> {
-        vec![registry.arg::<V1>(A::NAME, info)]
-    }
-
-    fn from_input_value(value: &InputValue<WundergraphScalarValue>) -> Option<(V1,)> {
-        V1::from_input_value(value).map(|v| (v,))
-    }
-
-    fn from_look_ahead(value: &LookAheadValue<WundergraphScalarValue>) -> Option<(V1,)> {
-        if let LookAheadValue::Object(ref o) = *value {
-            o.iter()
-                .find(|&(ref n, _)| *n == A::NAME)
-                .and_then(|(_, v)| V1::from_look_ahead(v))
-                .map(|v| (v,))
-        } else {
-            None
+macro_rules! primary_key_input_object_impl {
+    ($(
+        $Tuple:tt {
+            $(($idx: tt) -> $T:ident, $ST: ident, $TT: ident,)+
         }
-    }
+    )+) => {
+        $(
+            impl<$($T,)+ $($ST,)+ __I> PrimaryKeyInputObject<($($ST,)+), __I> for ($($T,)+)
+            where
+                $($T: Column,)+
+                $($ST: GraphQLType<WundergraphScalarValue, TypeInfo = __I>
+                  + FromInputValue<WundergraphScalarValue>
+                  + ToInputValue<WundergraphScalarValue>
+                  + FromLookAheadValue,)+
+            {
+                fn register<'r>(
+                    registry: &mut Registry<'r, WundergraphScalarValue>,
+                    info: &__I
+                ) -> Vec<Argument<'r, WundergraphScalarValue>> {
+                    vec![$(
+                        registry.arg::<$ST>($T::NAME, info),
+                    )+]
+                }
 
-    fn to_input_value(values: &(V1,)) -> InputValue<WundergraphScalarValue> {
-        let mut map = IndexMap::with_capacity(1);
-        map.insert(A::NAME, values.0.to_input_value());
-        InputValue::object(map)
+                fn from_input_value(value: &InputValue<WundergraphScalarValue>)
+                    -> Option<($($ST,)+)>
+                {
+                    $(
+                        let $ST = $ST::from_input_value(value)?;
+                    ) +
+                    Some(($($ST,)+))
+                }
+
+                fn from_look_ahead(value: &LookAheadValue<WundergraphScalarValue>)
+                    -> Option<($($ST, )+)>
+                {
+                    if let LookAheadValue::Object(ref o) = *value {
+                        $(
+                            let $ST = o.iter().find(|&(ref n, _)| *n == $T::NAME)
+                                .and_then(|(_, v)| $ST::from_look_ahead(v))?;
+                        ) +
+                        Some(($($ST,)+))
+                    } else {
+                        None
+                    }
+                }
+
+                fn to_input_value(values: &($($ST, )+)) -> InputValue<WundergraphScalarValue> {
+                    let mut map = IndexMap::with_capacity($Tuple);
+                    $(
+                        map.insert($T::NAME, values.$idx.to_input_value());
+                    )+
+                    InputValue::object(map)
+                }
+            }
+        )+
     }
 }
 
-impl<A, B, V1, V2, I> PrimaryKeyInputObject<(V1, V2), I> for (A, B)
-where
-    A: Column,
-    B: Column,
-    V1: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-    V2: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-{
-    fn register<'r>(
-        registry: &mut Registry<'r, WundergraphScalarValue>,
-        info: &I,
-    ) -> Vec<Argument<'r, WundergraphScalarValue>> {
-        vec![
-            registry.arg::<V1>(A::NAME, info),
-            registry.arg::<V2>(B::NAME, info),
-        ]
-    }
-
-    fn from_input_value(value: &InputValue<WundergraphScalarValue>) -> Option<(V1, V2)> {
-        V1::from_input_value(value).and_then(|v1| V2::from_input_value(value).map(|v2| (v1, v2)))
-    }
-
-    fn from_look_ahead(value: &LookAheadValue<WundergraphScalarValue>) -> Option<(V1, V2)> {
-        if let LookAheadValue::Object(ref o) = *value {
-            o.iter()
-                .find(|&(ref n, _)| *n == A::NAME)
-                .and_then(|(_, v)| V1::from_look_ahead(v))
-                .and_then(|v1| {
-                    o.iter()
-                        .find(|&(ref n, _)| *n == B::NAME)
-                        .and_then(|(_, v)| V2::from_look_ahead(v).map(|v2| (v1, v2)))
-                })
-        } else {
-            None
-        }
-    }
-
-    fn to_input_value(values: &(V1, V2)) -> InputValue<WundergraphScalarValue> {
-        let mut map = IndexMap::with_capacity(2);
-        map.insert(A::NAME, values.0.to_input_value());
-        map.insert(B::NAME, values.1.to_input_value());
-        InputValue::object(map)
-    }
-}
-
-impl<A, B, C, V1, V2, V3, I> PrimaryKeyInputObject<(V1, V2, V3), I> for (A, B, C)
-where
-    A: Column,
-    B: Column,
-    C: Column,
-    V1: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-    V2: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-    V3: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-{
-    fn register<'r>(
-        registry: &mut Registry<'r, WundergraphScalarValue>,
-        info: &I,
-    ) -> Vec<Argument<'r, WundergraphScalarValue>> {
-        vec![
-            registry.arg::<V1>(A::NAME, info),
-            registry.arg::<V2>(B::NAME, info),
-            registry.arg::<V3>(C::NAME, info),
-        ]
-    }
-
-    fn from_input_value(value: &InputValue<WundergraphScalarValue>) -> Option<(V1, V2, V3)> {
-        V1::from_input_value(value)
-            .and_then(|v1| V2::from_input_value(value).map(|v2| (v1, v2)))
-            .and_then(|(v1, v2)| V3::from_input_value(value).map(|v3| (v1, v2, v3)))
-    }
-
-    fn from_look_ahead(value: &LookAheadValue<WundergraphScalarValue>) -> Option<(V1, V2, V3)> {
-        if let LookAheadValue::Object(ref o) = *value {
-            o.iter()
-                .find(|&(ref n, _)| *n == A::NAME)
-                .and_then(|(_, v)| V1::from_look_ahead(v))
-                .and_then(|v1| {
-                    o.iter()
-                        .find(|&(ref n, _)| *n == B::NAME)
-                        .and_then(|(_, v)| V2::from_look_ahead(v).map(|v2| (v1, v2)))
-                }).and_then(|(v1, v2)| {
-                    o.iter()
-                        .find(|&(ref n, _)| *n == C::NAME)
-                        .and_then(|(_, v)| V3::from_look_ahead(v).map(|v3| (v1, v2, v3)))
-                })
-        } else {
-            None
-        }
-    }
-
-    fn to_input_value(values: &(V1, V2, V3)) -> InputValue<WundergraphScalarValue> {
-        let mut map = IndexMap::with_capacity(3);
-        map.insert(A::NAME, values.0.to_input_value());
-        map.insert(B::NAME, values.1.to_input_value());
-        map.insert(C::NAME, values.2.to_input_value());
-        InputValue::object(map)
-    }
-}
-
-impl<A, B, C, D, V1, V2, V3, V4, I> PrimaryKeyInputObject<(V1, V2, V3, V4), I> for (A, B, C, D)
-where
-    A: Column,
-    B: Column,
-    C: Column,
-    D: Column,
-    V1: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-    V2: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-    V3: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-    V4: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-{
-    fn register<'r>(
-        registry: &mut Registry<'r, WundergraphScalarValue>,
-        info: &I,
-    ) -> Vec<Argument<'r, WundergraphScalarValue>> {
-        vec![
-            registry.arg::<V1>(A::NAME, info),
-            registry.arg::<V2>(B::NAME, info),
-            registry.arg::<V3>(C::NAME, info),
-            registry.arg::<V4>(D::NAME, info),
-        ]
-    }
-
-    fn from_input_value(value: &InputValue<WundergraphScalarValue>) -> Option<(V1, V2, V3, V4)> {
-        V1::from_input_value(value)
-            .and_then(|v1| V2::from_input_value(value).map(|v2| (v1, v2)))
-            .and_then(|(v1, v2)| V3::from_input_value(value).map(|v3| (v1, v2, v3)))
-            .and_then(|(v1, v2, v3)| V4::from_input_value(value).map(|v4| (v1, v2, v3, v4)))
-    }
-
-    fn from_look_ahead(value: &LookAheadValue<WundergraphScalarValue>) -> Option<(V1, V2, V3, V4)> {
-        if let LookAheadValue::Object(ref o) = *value {
-            o.iter()
-                .find(|&(ref n, _)| *n == A::NAME)
-                .and_then(|(_, v)| V1::from_look_ahead(v))
-                .and_then(|v1| {
-                    o.iter()
-                        .find(|&(ref n, _)| *n == B::NAME)
-                        .and_then(|(_, v)| V2::from_look_ahead(v).map(|v2| (v1, v2)))
-                }).and_then(|(v1, v2)| {
-                    o.iter()
-                        .find(|&(ref n, _)| *n == C::NAME)
-                        .and_then(|(_, v)| V3::from_look_ahead(v).map(|v3| (v1, v2, v3)))
-                }).and_then(|(v1, v2, v3)| {
-                    o.iter()
-                        .find(|&(ref n, _)| *n == D::NAME)
-                        .and_then(|(_, v)| V4::from_look_ahead(v).map(|v4| (v1, v2, v3, v4)))
-                })
-        } else {
-            None
-        }
-    }
-
-    fn to_input_value(values: &(V1, V2, V3, V4)) -> InputValue<WundergraphScalarValue> {
-        let mut map = IndexMap::with_capacity(5);
-        map.insert(A::NAME, values.0.to_input_value());
-        map.insert(B::NAME, values.1.to_input_value());
-        map.insert(C::NAME, values.2.to_input_value());
-        map.insert(D::NAME, values.3.to_input_value());
-        InputValue::object(map)
-    }
-}
-
-impl<A, B, C, D, E, V1, V2, V3, V4, V5, I> PrimaryKeyInputObject<(V1, V2, V3, V4, V5), I>
-    for (A, B, C, D, E)
-where
-    A: Column,
-    B: Column,
-    C: Column,
-    D: Column,
-    E: Column,
-    V1: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-    V2: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-    V3: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-    V4: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-    V5: GraphQLType<WundergraphScalarValue, TypeInfo = I>
-        + FromInputValue<WundergraphScalarValue>
-        + ToInputValue<WundergraphScalarValue>
-        + FromLookAheadValue,
-{
-    fn register<'r>(
-        registry: &mut Registry<'r, WundergraphScalarValue>,
-        info: &I,
-    ) -> Vec<Argument<'r, WundergraphScalarValue>> {
-        vec![
-            registry.arg::<V1>(A::NAME, info),
-            registry.arg::<V2>(B::NAME, info),
-            registry.arg::<V3>(C::NAME, info),
-            registry.arg::<V4>(D::NAME, info),
-            registry.arg::<V5>(E::NAME, info),
-        ]
-    }
-
-    fn from_input_value(
-        value: &InputValue<WundergraphScalarValue>,
-    ) -> Option<(V1, V2, V3, V4, V5)> {
-        V1::from_input_value(value)
-            .and_then(|v1| V2::from_input_value(value).map(|v2| (v1, v2)))
-            .and_then(|(v1, v2)| V3::from_input_value(value).map(|v3| (v1, v2, v3)))
-            .and_then(|(v1, v2, v3)| V4::from_input_value(value).map(|v4| (v1, v2, v3, v4)))
-            .and_then(|(v1, v2, v3, v4)| V5::from_input_value(value).map(|v5| (v1, v2, v3, v4, v5)))
-    }
-
-    fn from_look_ahead(
-        value: &LookAheadValue<WundergraphScalarValue>,
-    ) -> Option<(V1, V2, V3, V4, V5)> {
-        if let LookAheadValue::Object(ref o) = *value {
-            o.iter()
-                .find(|&(ref n, _)| *n == A::NAME)
-                .and_then(|(_, v)| V1::from_look_ahead(v))
-                .and_then(|v1| {
-                    o.iter()
-                        .find(|&(ref n, _)| *n == B::NAME)
-                        .and_then(|(_, v)| V2::from_look_ahead(v).map(|v2| (v1, v2)))
-                }).and_then(|(v1, v2)| {
-                    o.iter()
-                        .find(|&(ref n, _)| *n == C::NAME)
-                        .and_then(|(_, v)| V3::from_look_ahead(v).map(|v3| (v1, v2, v3)))
-                }).and_then(|(v1, v2, v3)| {
-                    o.iter()
-                        .find(|&(ref n, _)| *n == D::NAME)
-                        .and_then(|(_, v)| V4::from_look_ahead(v).map(|v4| (v1, v2, v3, v4)))
-                }).and_then(|(v1, v2, v3, v4)| {
-                    o.iter()
-                        .find(|&(ref n, _)| *n == E::NAME)
-                        .and_then(|(_, v)| V5::from_look_ahead(v).map(|v5| (v1, v2, v3, v4, v5)))
-                })
-        } else {
-            None
-        }
-    }
-
-    fn to_input_value(values: &(V1, V2, V3, V4, V5)) -> InputValue<WundergraphScalarValue> {
-        let mut map = IndexMap::with_capacity(5);
-        map.insert(A::NAME, values.0.to_input_value());
-        map.insert(B::NAME, values.1.to_input_value());
-        map.insert(C::NAME, values.2.to_input_value());
-        map.insert(D::NAME, values.3.to_input_value());
-        map.insert(E::NAME, values.4.to_input_value());
-        InputValue::object(map)
-    }
-}
+__diesel_for_each_tuple!(primary_key_input_object_impl);
 
 #[derive(Debug)]
 pub struct PrimaryKeyInfo(String);
