f781c3210ab0ee55bd95d99d3522942afc6a0066
Alexei Pastuchov <alexei.pastuchov@pyur.com>
2020-05-02 08:07:38 +0200

    Implement ApplyOffset for diesel::mysql::Mysq

--- wundergraph/src/query_builder/selection/offset.rs
+++ wundergraph/src/query_builder/selection/offset.rs
@@ -1,15 +1,16 @@
 use crate::error::Result;
-#[cfg(any(feature = "postgres", feature = "sqlite"))]
+#[cfg(any(feature = "postgres", feature = "sqlite", feature = "mysql"))]
 use crate::error::WundergraphError;
-#[cfg(any(feature = "postgres", feature = "sqlite"))]
+#[cfg(any(feature = "postgres", feature = "sqlite", feature = "mysql"))]
 use crate::juniper_ext::FromLookAheadValue;
 use crate::query_builder::selection::{BoxedQuery, LoadingHandler};
 use crate::scalar::WundergraphScalarValue;
 use diesel::backend::Backend;
-#[cfg(feature = "sqlite")]
+#[cfg(any(feature = "sqlite", feature = "mysql"))]
 use diesel::query_dsl::methods::LimitDsl;
-#[cfg(any(feature = "postgres", feature = "sqlite"))]
+#[cfg(any(feature = "postgres", feature = "sqlite", feature = "mysql"))]
 use diesel::query_dsl::methods::OffsetDsl;
+
 use juniper::LookAheadSelection;
 
 /// A trait abstracting over the different behaviour of limit/offset
@@ -66,6 +67,33 @@ impl ApplyOffset for diesel::sqlite::Sqlite {
                 Ok(q)
             } else {
                 Ok(<_ as LimitDsl>::limit(q, -1))
+            }
+        } else {
+            Ok(query)
+        }
+    }
+}
+
+#[cfg(feature = "mysql")]
+impl ApplyOffset for diesel::mysql::Mysql {
+    fn apply_offset<'a, L, Ctx>(
+        query: BoxedQuery<'a, L, Self, Ctx>,
+        select: &LookAheadSelection<'_, WundergraphScalarValue>,
+    ) -> Result<BoxedQuery<'a, L, Self, Ctx>>
+    where
+        L: LoadingHandler<Self, Ctx>,
+    {
+        use juniper::LookAheadMethods;
+        if let Some(offset) = select.argument("offset") {
+            let q = <_ as OffsetDsl>::offset(
+                query,
+                i64::from_look_ahead(offset.value())
+                    .ok_or(WundergraphError::CouldNotBuildFilterArgument)?,
+            );
+            if select.argument("limit").is_some() {
+                Ok(q)
+            } else {
+                Ok(<_ as LimitDsl>::limit(q, std::i64::MAX))
             }
         } else {
             Ok(query)
